1d0fee7f195aa432eff768707cef90a84856cfe7
Ramish Hassan <ramishshah93@gmail.com>
2023-01-12 21:45:14 +0100

    user schema and post route done and working

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,9 +1,8 @@
 import express from "express";
+import { register } from "../controllers/authControl.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("this is auth endpoint");
-});
+router.post("/register", register);
 
 export default router;
