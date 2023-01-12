11832fed12bb016e1786d7a32a1214044cbe4035
Ramish Hassan <ramishshah93@gmail.com>
2023-01-12 20:28:08 +0100

    put route working as expected

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -15,4 +15,20 @@ router.post("/", async (req, res) => {
   }
 });
 
+//update route
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedHotel);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 export default router;
