7feaf388e096c71c6bc4269595b6f713d6e96aab
Ramish Hassan <ramishshah93@gmail.com>
2023-01-13 13:03:15 +0100

    all CRUD routes for user complete along with their controllers in seperate file, testing jwt next

--- api/controllers/authControl.js
+++ api/controllers/authControl.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 import User from "../models/User.js";
 
@@ -39,7 +40,12 @@ export const login = async (req, res, next) => {
     );
 
     const { password, isAdmin, ...otherDetails } = user._doc;
-    res.status(200).json({ ...otherDetails });
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json({ ...otherDetails });
   } catch (error) {
     next(error);
   }
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,16 @@
 import express from "express";
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from "../controllers/userControl.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("this is users endpoint");
-});
+router.get("/:id", getUser);
+router.get("/", getUsers);
+router.delete("/:id", deleteUser);
+router.put("/:id", updateUser);
 
 export default router;
