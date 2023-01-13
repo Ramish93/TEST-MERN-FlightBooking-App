811f43a5c0396b6c44d665d2c0d771a77bf3ec85
Ramish Hassan <ramishshah93@gmail.com>
2023-01-13 12:26:09 +0100

    password in authcontrol hashing

--- api/controllers/authControl.js
+++ api/controllers/authControl.js
@@ -1,11 +1,17 @@
+import bcrypt from "bcryptjs";
+
 import User from "../models/User.js";
 
 export const register = async (req, res, next) => {
   try {
+    // hashing and salting password with bcrypt
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hash,
     });
 
     await newUser.save();
